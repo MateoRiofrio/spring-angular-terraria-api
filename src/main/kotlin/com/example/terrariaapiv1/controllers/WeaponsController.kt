1ecc564e5d7940776f67b7f18b4655935a831dd5
unknown <materiochi123@gmail.com>
2021-08-21 12:51:50 -0400

    change api structure

--- src/main/kotlin/com/example/terrariaapiv1/controllers/WeaponsController.kt
+++ src/main/kotlin/com/example/terrariaapiv1/controllers/WeaponsController.kt
@@ -8,20 +8,20 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/weapons")
+@RequestMapping("/api/v1")
 class WeaponsController(private val weaponRepository: WeaponRepository) {
     /**
-     * Mapping of 'api/weapons' returns all, or sorts by field in ascending or descending order
+     * Mapping of 'api/v1/weapons' returns all, or sorts by field in ascending or descending order
      * Params:
-     *  sort_by -> field from Weapon type
+     *  sort_by -> field from Weapon type to sort by
      *  order -> 'ORDER BY' direction
      * Default Behavior: All weapons by id in ascending order
      */
-    @GetMapping
+    @GetMapping("/weapons")
     fun sortByFieldOrderBy(
         @RequestParam(value = "sort_by", defaultValue = "id") field: String,
         @RequestParam(value = "order", defaultValue = "asc") order: String
-    ): ResponseEntity<List<Weapon>>  {
+    ): ResponseEntity<List<Weapon>> {
         val item = when (order) {
             "asc" -> weaponRepository.findWeaponsBy(Sort.by(Sort.Direction.ASC, field))
             "desc" -> weaponRepository.findWeaponsBy(Sort.by(Sort.Direction.DESC, field))
@@ -29,14 +29,15 @@ class WeaponsController(private val weaponRepository: WeaponRepository) {
         }
         return ResponseEntity.ok(item)
     }
+
     // Get a weapon by name
-    @GetMapping("/{name}")
+    @GetMapping("weapons/item_name/{name}")
     fun getByItemName(@PathVariable name: String): ResponseEntity<List<Weapon>> {
         return ResponseEntity.ok(weaponRepository.findByItemName(name))
     }
 
     // Get a weapon from a LIKE query (must provide 'like' parameter)
-    @GetMapping("/item_name")
+    @GetMapping("weapons/item_name")
     fun getItemByLike(@RequestParam(required = true, value = "like") like: String): ResponseEntity<List<Weapon>> {
         return ResponseEntity.ok(weaponRepository.findByItemNameStartsWith(like))
     }

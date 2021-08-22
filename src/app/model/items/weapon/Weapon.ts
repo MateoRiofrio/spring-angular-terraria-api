da53ebb07435e7fd9399b6ed04151222356d8fac
unknown <materiochi123@gmail.com>
2021-08-22 18:12:29 -0400

    add id field

---
+++ src/app/model/items/weapon/Weapon.ts
@@ -0,0 +1,18 @@
+export interface Weapon {
+    id: number
+    itemName: string
+    usesAmmo: string
+    damage: number
+    knockback: number
+    consumable: number
+    criticalChance: number
+    useTime: number
+    velocity: number
+    toolip: string
+    maxStack: number
+    rarity: string
+    buy: number
+    sell: number
+    research: number
+    itemImgUrl: string
+}

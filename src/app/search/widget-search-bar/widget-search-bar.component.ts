eac0f9afbafbe5f7fedc701a9010b7bb54a616ec
unknown <materiochi123@gmail.com>
2021-08-22 18:15:17 -0400

    add formatting functions and initalizer function to search bar

--- src/app/search/widget-search-bar/widget-search-bar.component.ts
+++ src/app/search/widget-search-bar/widget-search-bar.component.ts
@@ -1,31 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {Weapon} from "../../model/items/Weapon";
+import {Component, Input, OnInit} from '@angular/core';
+import {Weapon} from "../../model/items/weapon/Weapon";
 import {SearchService} from "../search.service";
 import {Router} from "@angular/router";
-import {Observable} from "rxjs";
+
 
 @Component({
     selector: 'app-widget-search-bar',
     templateUrl: 'widget-search-bar.component.html',
-    styles: []
+    styleUrls: ['widget-search-bar.component.css']
 })
-export class WidgetSearchBarComponent implements OnInit {
+export class WidgetSearchBarComponent implements OnInit{
 
-    search_results$: Observable<Weapon[]>;
+    public searchResults: Weapon[] = []
+    showList: boolean = false
+
     // inject http search service
     constructor(private router: Router, private searchService: SearchService) {
-        this.search_results$ = this.searchService.findAll();
     }
 
-    findAll() {
-
+    showStartsWithResults(event: any) {
+        this.showList = true
+        // show the list make a LIKE query on every current input in text field
+        this.searchService.findStartsWith(event.target.value).subscribe(data => this.searchResults = data)
     }
-    onSubmit(f: NgForm) {
-        console.log("onSubmit from search bar")
+    parseName(name: string) {
+        // use regular expression with /g flag to replace all instances of character '_'
+        // source: https://stackoverflow.com/questions/2116558/fastest-method-to-replace-all-instances-of-a-character-in-a-string/2116614
+        return name.replace(/_/g, " ")
     }
     ngOnInit() {
-        this.router.events.subscribe(value => this.findAll())
+        // show all weapons by default
+        this.searchService.findAll().subscribe(data => {
+            this.searchResults = data
+        })
     }
 
 }

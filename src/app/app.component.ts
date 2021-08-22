36515993f98f7fc2c2b8b0822dc6cded2711b1fe
unknown <materiochi123@gmail.com>
2021-08-22 12:54:36 -0400

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styles: []
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styles: []
 })
 export class AppComponent {
-  title = 'Terraria API';
-  constructor(private http: HttpClient) {
+    title = 'Terraria API';
 
-  }
+    constructor(private http: HttpClient) {
+
+    }
 
 }

5d0084b4bf02f7b181ea0b64ef1b04ec01261b5b
unknown <materiochi123@gmail.com>
2021-08-22 18:13:10 -0400

    add routing to weapon details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,30 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {SearchResultItemComponent} from './search/search-result-item/search-result-item.component';
 import {WidgetSearchBarComponent} from './search/widget-search-bar/widget-search-bar.component';
-import {SearchResultListComponent} from './search/search-result-list/search-result-list.component';
 import {HttpClientModule} from '@angular/common/http';
 import {SearchService} from "./search/search.service";
 import {CommonModule} from '@angular/common';
 import {MatListModule} from '@angular/material/list';
+import { WeaponDetailsComponent } from './model/items/weapon/weapon-details/weapon-details.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        SearchResultItemComponent,
         WidgetSearchBarComponent,
-        SearchResultListComponent
+        WeaponDetailsComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
         CommonModule,
-        MatListModule
+        MatListModule,
+        RouterModule.forRoot([
+            { path: 'weapons/:id', component: WeaponDetailsComponent },
+        ])
     ],
     providers: [SearchService],
     bootstrap: [AppComponent]

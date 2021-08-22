dc9c6c27c1ed62c769199bf3ebd655f2b8518888
unknown <materiochi123@gmail.com>
2021-08-22 12:00:14 -0400

    add weapon model for angular

--- src/app/model/items/Weapon.ts
+++ src/app/model/items/Weapon.ts
@@ -1,4 +1,18 @@
 export interface Weapon {
-    item_name: string;
-    item_img_url: string;
+    itemName: string,
+    usesAmmo: string,
+    damage: number,
+    knockback: number,
+    consumable: number,
+    criticalChance: number,
+    useTime: number,
+    velocity: number,
+    toolip: string,
+    maxStack: number,
+    rarity: string,
+    buy: number,
+    sell: number,
+    research: number,
+    itemImgUrl: string
+
 }
